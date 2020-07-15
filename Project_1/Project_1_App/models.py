2386819e38b36993544f89b74bd81e8ff18f1018
unknown <kubaslawski@gmail.com>
2020-07-15 21:12:27 +0200

    Creating models

--- Project_1/Project_1_App/models.py
+++ Project_1/Project_1_App/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+
+
+type_of_organization =  (
+    (1, "Fundacja"),
+    (2, "Organizacja pozarządowa"),
+    (3, "Zbiórka lokalna"),
+)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=256)
+    type = models.CharField(choices=type_of_organization, default=1, max_length=32)
+    categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=128)
+    phone_number = models.CharField(max_length=16)
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=16)
+    pick_up_date = models.DateField(null=True, blank=True)
+    pick_up_time = models.DateTimeField(null=True, blank=True)
+    pick_up_comment = models.CharField(max_length=256)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
